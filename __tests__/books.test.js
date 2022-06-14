c37c170c381af3a33780e4b0abad421a1f48f318
Khayman King <khaymanak@gmail.com>
2022-06-14 09:56:41 -0700

    test for get all mostly passes

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -8,7 +8,7 @@ describe('books routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('/books/:id should return a list of book by id', async() => {
+  xit('/books/:id should return a list of book by id', async() => {
     const res = await request(app).get('/books/1');
     expect(res.body).toEqual({
       'id': '1',
@@ -17,11 +17,15 @@ describe('books routes', () => {
       'release': '1999-09-09'
     });
   });
-  xit('/books should return a list of books', async() => {
+  it('/books should return a list of books', async() => {
     const res = await request(app).get('/books');
     const books = await Book.getAll();
     const expected = books.map((book) => {
-      return { id: book.id, title: book.title };
+      return { 
+        id: book.id, 
+        title: book.title, 
+        publisher: book.publisher, 
+        release: book.release };
     });
     expect(res.body).toEqual(expected);
   });
