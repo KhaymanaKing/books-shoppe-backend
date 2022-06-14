ea1ada531b820278a0de561567e7ab8eaffe868a
Khayman King <khaymanak@gmail.com>
2022-06-14 10:50:07 -0700

    added get all authors test

--- __tests__/authors.test.js
+++ __tests__/authors.test.js
@@ -4,7 +4,22 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('author routes', () => {
-    beforE(() => {
-
-    })
-})
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('/authors should return a list of authors', async() => {
+    const res = await request(app).get('/authors');
+    const authors = await Author.getAll();
+    const expected = authors.map((author) => {
+      return{
+        id: authors.id,
+        name: authors.name,
+        dob: authors.dob,
+        pob: authors.pob };
+    });
+    expect(res.body).toEqueal(expected);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
